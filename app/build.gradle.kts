ff8b6fc44cf3e171d4b288b99e844f65f91ab94c
Alexander Martinez Moron <amartinez@greensidesolutions.com>
2025-06-28 21:56:36 +0200

    firebase.ai

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.ai)
     // Google Identity Services para One Tap Sign-In
     implementation("com.google.android.gms:play-services-auth:21.3.0")
 
